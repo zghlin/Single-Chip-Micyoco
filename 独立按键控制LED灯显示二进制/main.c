84c7e78482df5725b8cb0822d33ff329efa83eda
zghlin <108162823+zghlin@users.noreply.github.com>
2023-07-19 11:47:34 +0800

    Add files via upload

---
+++ 独立按键控制LED灯显示二进制/main.c
@@ -0,0 +1,28 @@
+#include <REGX52.H>
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms--)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
+
+void main()
+{
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay1ms(20);
+			while(P3_1==0);
+			Delay1ms(20);
+			P2++;
+		}
+	}
+}
