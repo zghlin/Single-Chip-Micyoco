97b5489c8eac991ce239eafea8b45ba076f7005e
zghlin <108162823+zghlin@users.noreply.github.com>
2023-07-19 17:25:04 +0800

    Add files via upload

---
+++ 静态数码管显示1/main.c
@@ -0,0 +1,24 @@
+#include <REGX52.H>
+void Nexie(unsigned char Location,Number)
+{
+	switch(Location)
+	{
+		case 1:P2_4=1;P2_3=1;P2_2=1;break;
+		case 2:P2_4=1;P2_3=1;P2_2=0;break;
+		case 3:P2_4=1;P2_3=0;P2_2=1;break;
+		case 4:P2_4=1;P2_3=0;P2_2=0;break;
+		case 5:P2_4=0;P2_3=1;P2_2=1;break;
+		case 6:P2_4=0;P2_3=1;P2_2=0;break;
+		case 7:P2_4=0;P2_3=0;P2_2=1;break;
+		case 8:P2_4=0;P2_3=0;P2_2=0;break;
+	}
+	P0=0x7D;
+}
+void main()
+{
+		Nexie(7,3);
+		while(1)
+		{
+		
+		}
+}
