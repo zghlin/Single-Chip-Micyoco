b96eb1d0c68a86915ce41702ebd59467b0312caf
zghlin <108162823+zghlin@users.noreply.github.com>
2023-07-19 10:43:59 +0800

    Add files via upload

---
+++ 独立按键控制灯泡亮灭2/main.c
@@ -0,0 +1,30 @@
+#include<REGX52.H>
+#include<INTRINS.H>
+void Delay1ms(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms)
+	{
+		i = 2;
+	j = 239;
+	do
+	{
+		while (--j);
+	} while (--i);
+		xms--;
+	}
+}
+
+void main()
+{
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay1ms(20);
+			while(P3_1==0);
+			Delay1ms(20);
+			P2_0=~P2_0;
+		}
+	}
+}
