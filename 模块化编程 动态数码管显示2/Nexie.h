33705bf48def45c3a36e04a483fe0dbdd98679b8
zghlin <108162823+zghlin@users.noreply.github.com>
2023-07-20 14:19:36 +0800

    Add files via upload

---
+++ 模块化编程 动态数码管显示2/Nexie.h
@@ -0,0 +1,4 @@
+#ifndef _Nexie_H_
+#define _Nexie_H_
+void Nexie(unsigned char Location,Number);
+#endif
---
+++ 模块化编程 动态数码管显示2/main.c
@@ -0,0 +1,26 @@
+#include <REGX52.H>
+#include<Delay.H>
+#include<Nexie.H>
+void main()
+{
+		while(1)
+		{
+			Nexie(1,1);
+			Delay(1);
+			Nexie(2,2);
+			Delay(1);
+			Nexie(3,3);
+			Delay(1);
+			Nexie(4,4);
+			Delay(1);
+			Nexie(5,5);
+			Delay(1);
+			Nexie(6,6);
+			Delay(1);
+			Nexie(7,7);
+			Delay(1);
+			Nexie(8,9);
+			Delay(1);
+			
+		}
+}
